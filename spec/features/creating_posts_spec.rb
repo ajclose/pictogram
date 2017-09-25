ef828cec898c301fb5fa76a41b3e73f135369521
Alex Close <ajclose@gmail.com>
2017-09-24 20:04:07 -0500

    Login and logout

--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper.rb'
 
 feature 'Creating posts' do
-  scenario 'Can create a post' do
+  background do
+    user = create(:user)
     visit '/'
+    click_link 'Login'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+  end
+  scenario 'Can create a post' do
     click_link 'New Post'
     attach_file('Image', "spec/files/images/coffee.jpg")
     fill_in 'Caption', with: "nom nom nom #coffeetime"
@@ -12,7 +19,6 @@ feature 'Creating posts' do
   end
 
     it 'Post requires image' do
-    visit '/'
     click_link 'New Post'
     fill_in 'Caption', with: "no image!"
     click_button 'Create Post'
